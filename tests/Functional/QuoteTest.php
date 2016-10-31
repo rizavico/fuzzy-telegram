c37415fc54c18c37b36f2cf83a10a83fc850a960
Ali Rizavi <ali@rizavi.co>
2016-10-31 22:16:14 +0000

    Committing broken phpunit test

--- tests/Functional/QuoteTest.php
+++ tests/Functional/QuoteTest.php
@@ -7,7 +7,7 @@ class QuoteTest extends BaseTestCase
     public function testNumberOfCategories(){
         $quote = new \Quote(null, null);
         $categories = $quote->getCategories();
-        $this->assertEquals(5, sizeof($categories));
+        $this->assertEquals(8, sizeof($categories));
     }
 
     public function testCategoryListContainsManagement(){
